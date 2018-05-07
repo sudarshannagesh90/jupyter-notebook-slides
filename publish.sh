f4d3c56fc083f2c9a58851844673bf1a4cc89336
Sudarshan Nagesh <sudarshannagesh90@gmail.com>
2018-05-06 21:43:35 -0500

    Update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
-mv slides.slides.html  index.html
+jupyter-nbconvert --to slides Chapter1ImageDeconvolution1.ipynb --reveal-prefix=reveal.js
+mv Chapter1ImageDeconvolution1.slides.html  index.html
 mkdir -p /tmp/workspace
 cp -r * /tmp/workspace/
 git add -A .
